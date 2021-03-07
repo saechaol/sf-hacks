d8fe52b8f38f0256b1051a553a3b231d9aa0343f
Ninh Le <ninh.le.94@gmail.com>
2021-03-06 20:21:43 -0800

    Voice

--- components/Menu.js
+++
@@ -1,1 +0,0 @@
-// @TODO Menu Pop out here
--- pages/Translate.js
+++ pages/Translate.js
@@ -1,27 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import VoiceTest from '../components/VoiceTest'
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
-import Voice from '@react-native-voice/voice';
 
-class VoiceTest extends React.Component {
-  constructor(props) {
-    Voice.onSpeechStart = this.onSpeechStartHandler.bind(this);
-    Voice.onSpeechEnd = this.onSpeechEndHandler.bind(this);
-    Voice.onSpeechResults = this.onSpeechResultsHandler.bind(this);
-  }
-  onStartButtonPress(e){
-    Voice.start('en-US');
-  }
-}
 
 const Translate = (e) => {
   return (
     <View style={styles.container}>
-      <TouchableWithoutFeedback onPress={() =>
-        VoiceTest.onStartButtonPress(e)
-      }>
-        <Text>Translate</Text>
-      </TouchableWithoutFeedback>
+      <VoiceTest />
       <StatusBar style="auto" />
     </View>
   );
